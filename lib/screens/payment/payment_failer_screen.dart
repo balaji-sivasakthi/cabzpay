18e207d536788b6c4573116f8dc4612278236842
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2022-08-18 00:43:55 +0530

    payment page

---
+++ lib/screens/payment/payment_failer_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class PaymentFailureScreen extends StatefulWidget {
+  const PaymentFailureScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PaymentFailureScreen> createState() => _PaymentFailureScreenState();
+}
+
+class _PaymentFailureScreenState extends State<PaymentFailureScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(color: Color.fromRGBO(2, 41, 100, 1)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              
+              
+            Stack(
+              clipBehavior: Clip.none,
+              children: [
+              Positioned(child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 500,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white
+                ),
+                child:Container(child: Center(child: Text("Hello"),)),
+              )),
+              Positioned(
+                top: 400/2,
+                left: -50,
+                child: Container(
+                width: 70,
+                height: 70,
+               decoration: BoxDecoration(
+                color: Color.fromRGBO(2, 41, 100, 1),
+                borderRadius: BorderRadius.circular(100)
+                ),
+              )),
+              Positioned(
+                top: 400/2,
+                right: -50,
+                child: Container(
+                width: 70,
+                height: 70,
+               decoration: BoxDecoration(
+                color: Color.fromRGBO(2, 41, 100, 1),
+                borderRadius: BorderRadius.circular(100)
+                ),
+              )),
+              Positioned(
+                top: -50,
+                left: 100,
+                child: Container(
+                width: 150,
+                height: 150,
+               decoration: BoxDecoration(
+                color: Color.fromRGBO(55, 132, 251, .2),
+                borderRadius: BorderRadius.circular(100)
+                ),
+              )), Positioned(
+                top: -30,
+                left: 125,
+                child: Container(
+                  child: Center(child: Icon(Icons.check,color: Colors.white,size: 48,)),
+                width: 100,
+                height: 100,
+               decoration: BoxDecoration(
+                 color: Color.fromRGBO(55, 132, 251, 1),
+                borderRadius: BorderRadius.circular(100)
+                ),
+              ))
+            ],),
+            Container(
+                  height: 50,
+                  child: Center(
+                    child: Text(
+                      "Back to home",
+                      style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 16),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                ),
+          ],),
+      ),
+    );
+  }
+}
